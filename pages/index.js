00e3fde44c11f77d6922fa2e803a1f34f0927396
HorusWan <hwan17@my.bcit.ca>
2023-03-13 16:43:05 -0700

    homepagecss commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
 import Nav from "../comps/navbar";
 import { Text } from "@nextui-org/react";
+import { Moviemenu } from "../comps/menumovie";
+import { Gamemenu } from "../comps/menugame";
+import { Menutitlecard } from "../comps/menutitlecard";
+
+const containerStyle = {
+  display: "grid",
+  gridTemplateColumns: "1fr 1fr",
+  alignItems: "center",
+  minHeight: "100vh"
+};
+
+const menuStyle = {
+  width: "100%",
+  height: "100%",
+  gridRow: "2" // Add this property to avoid overlapping with Menutitlecard
+};
 
 export default function Home() {
   const text = "E-SPOT is a entertainment web app using RAWG.io & themoviedb.org api to view trending Games and Movies"
@@ -7,37 +23,16 @@ export default function Home() {
     <div>
       <Nav/>
       <div>
-      <Text
-        h1
-        size={60}
-        css={{
-          textAlign: "center"
-        }}
-      >
-        Welcome to
-      </Text>
-      <Text
-        h1
-        size={60}
-        css={{
-          color: "$red700",
-          textAlign: "center"
-        }}
-        weight="bold"
-      >
-        E-SPOT !
-      </Text>
-
-      <Text 
-      css={{
-        textAlign: "center"
-      }}
-      >{text}</Text>
-
-
-
+        <Menutitlecard/>
+        <div style={containerStyle}>
+          <div style={menuStyle}>
+            <Moviemenu />
+          </div>
+          <div style={menuStyle}>
+            <Gamemenu />
+          </div>
+        </div>
       </div>
-      
     </div>
     
   );
